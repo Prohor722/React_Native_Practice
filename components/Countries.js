b2359d31b5fee0ccb434b6351b76374aa5993a91
Prohor722 <prohor.b722@gmail.com>
2022-05-31 23:10:02 +0600

    coutries api connect and data length show

--- components/Countries.js
+++ components/Countries.js
@@ -4,13 +4,13 @@ import React, {useState, useEffect} from 'react'
 export default function countries() {
     const [countries, setCountries] = useState([]);
     useEffect(()=>{
-        fetch('')
+        fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountries(data))
     },[])
   return (
     <View style={{backgroundColor: "red"}} >
-      <Text>countries</Text>
+      <Text>countries {countries.length}</Text>
     </View>
   )
 }
